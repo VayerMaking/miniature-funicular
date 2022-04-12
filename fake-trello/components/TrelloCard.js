29e1bece6f103cb196d46518cf09f2b21ad2208c
Martin Vayer <martin.vayer@gmail.com>
2022-04-12 10:14:11 +0300

    add basic cards

---
+++ fake-trello/components/TrelloCard.js
@@ -0,0 +1,21 @@
+import { Card, Text, Image, Group, Button, Badge } from "@mantine/core";
+
+export default function TrelloCard(props) {
+  return (
+    <Card style={{ width: 200, marginBottom: 10 }} shadow="sm" p="lg">
+      <Group position="apart" style={{ marginBottom: 5 }}>
+        <Text weight={500}>{props.cardTitle}</Text>
+        {/* TODO dynamicaly load colors for labels from props */}
+        <Badge color="pink" variant="light"></Badge>
+      </Group>
+
+      <Text size="sm" style={{ lineHeight: 1.5 }}>
+        {props.cardContent}
+      </Text>
+
+      <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
+        save
+      </Button>
+    </Card>
+  );
+}
