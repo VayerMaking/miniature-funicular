1d11ec22be41227535a2a46002456b0bb465be93
Martin Vayer <martin.vayer@gmail.com>
2022-04-13 14:24:34 +0300

    add expanded card component for editing

--- fake-trello/src/components/ExpandedCard.js
+++ fake-trello/src/components/ExpandedCard.js
@@ -1,12 +1,36 @@
-import { Card, Text, Image, Group, Button, Badge } from "@mantine/core";
+import { Card, Text, TextInput, Group, Button, Badge } from "@mantine/core";
 import { useParams } from "react-router-dom";
 
-export default function ExpandedCard() {
+import { useState, useEffect } from "react";
+import { useIndexedDB } from "react-indexed-db";
+
+export default function ExpandedCard(props) {
+  const [card, setCard] = useState([]);
+  const [input, setInput] = useState("");
+
   const { id } = useParams();
-  console.log("id", id);
   return (
-    <div>
-      <h1>expandend {id}</h1>
-    </div>
+    <Card style={{ width: 400, marginBottom: 10 }} shadow="sm" p="lg">
+      <Group position="apart" style={{ marginBottom: 5 }}>
+        <Text weight={500}>expanded {props.cardTitle}</Text>
+        {/* TODO dynamicaly load colors for labels from props */}
+        <Badge color="pink" variant="light"></Badge>
+      </Group>
+
+      <Text size="sm" style={{ lineHeight: 1.5 }}>
+        {props.cardContent}
+      </Text>
+      <TextInput value={input} onChange={(e) => setInput(e.target.value)} />
+
+      <Button
+        variant="light"
+        color="blue"
+        fullWidth
+        style={{ marginTop: 14 }}
+        onClick={console.log("saved")}
+      >
+        save
+      </Button>
+    </Card>
   );
 }
