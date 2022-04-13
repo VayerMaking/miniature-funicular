8cd6cb4cb16e1ce9744ef8d7a991e64453712495
Martin Vayer <martin.vayer@gmail.com>
2022-04-13 14:25:56 +0300

    displaying all cards and the create new card comp

--- fake-trello/src/routes/Cards.js
+++ fake-trello/src/routes/Cards.js
@@ -1,10 +1,24 @@
 import React from "react";
+import NewTrelloCard from "../components/NewTrelloCard";
 import TrelloCard from "../components/TrelloCard";
+import { useState, useEffect } from "react";
+import { useIndexedDB } from "react-indexed-db";
 
 const Cards = () => {
+  const [cards, setCards] = useState([]);
+  const { getAll } = useIndexedDB("cards");
+  useEffect(() => {
+    getAll().then((cardsFromDB) => {
+      setCards(cardsFromDB);
+    });
+  }, []);
+
   return (
     <div>
-      <TrelloCard cardTitle="1" cardContent="asdadsasd" cardId="1" />
+      <NewTrelloCard />
+      {cards.map((card) => (
+        <TrelloCard card={card} />
+      ))}
     </div>
   );
 };
