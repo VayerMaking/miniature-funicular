1f2bc430963198ecbd3b25278c5e9e6dac2bbcc9
Martin Vayer <martin.vayer@gmail.com>
2022-04-13 16:58:42 +0300

    add option to edit card

--- fake-trello/src/components/ExpandedCard.js
+++ fake-trello/src/components/ExpandedCard.js
@@ -1,33 +1,50 @@
-import { Card, Text, TextInput, Group, Button, Badge } from "@mantine/core";
+import { Card, Text, Textarea, Group, Button, Badge } from "@mantine/core";
 import { useParams } from "react-router-dom";
 
 import { useState, useEffect } from "react";
 import { useIndexedDB } from "react-indexed-db";
 
-export default function ExpandedCard(props) {
-  const [card, setCard] = useState([]);
-  const [input, setInput] = useState("");
-
+export default function ExpandedCard() {
+  const [card, setCard] = useState({});
+  const [input, setInput] = useState();
   const { id } = useParams();
+  const { update, getByID } = useIndexedDB("cards");
+  function updateCard(input) {
+    update({ id: card.id, title: card.title, content: input }).then((error) => {
+      console.log(error);
+    });
+  }
+  useEffect(() => {
+    getByID(id).then((card) => {
+      setCard(card);
+      console.log("card in use", card);
+    });
+
+    setInput(card.content);
+  }, []);
   return (
     <Card style={{ width: 400, marginBottom: 10 }} shadow="sm" p="lg">
       <Group position="apart" style={{ marginBottom: 5 }}>
-        <Text weight={500}>expanded {props.cardTitle}</Text>
-        {/* TODO dynamicaly load colors for labels from props */}
         <Badge color="pink" variant="light"></Badge>
       </Group>
 
       <Text size="sm" style={{ lineHeight: 1.5 }}>
-        {props.cardContent}
+        {card.title}
       </Text>
-      <TextInput value={input} onChange={(e) => setInput(e.target.value)} />
+      <Textarea
+        value={input}
+        placeholder={input}
+        onChange={(e) => setInput(e.target.value)}
+        autosize
+        minRows={2}
+      />
 
       <Button
         variant="light"
         color="blue"
         fullWidth
         style={{ marginTop: 14 }}
-        onClick={console.log("saved")}
+        onClick={() => updateCard(input)}
       >
         save
       </Button>
