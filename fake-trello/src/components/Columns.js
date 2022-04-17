d51d886d186bd235579cf0b9dd11785eb8404932
GerganaRoeva <gergana.roeva@gmail.com>
2022-04-17 21:16:25 +0300

    format

--- fake-trello/src/components/Columns.js
+++ fake-trello/src/components/Columns.js
@@ -1,23 +1,15 @@
-import { Grid } from "@mantine/core";
-import { Card, Image, Text } from '@mantine/core';
+import { Card } from "@mantine/core";
 
-import { SimpleGrid } from '@mantine/core';
-
+import { SimpleGrid } from "@mantine/core";
 
 export default function Column(props) {
   return (
     <SimpleGrid>
-        <Card
-      shadow="sm"
-      p="xl"
-      component="a"
-      target="_blank"
-    >
-      <Card.Section>
+      <Card shadow="sm" p="xl" component="a" target="_blank">
+        <Card.Section>
           <p>column id: {props.columns}</p>
-      </Card.Section>
-    </Card> 
+        </Card.Section>
+      </Card>
     </SimpleGrid>
-      
   );
 }
