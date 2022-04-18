1be6d6c82f4084b9cabd0f6d32a2aa59afa4e79c
GerganaRoeva <gergana.roeva@gmail.com>
2022-04-18 09:39:22 +0300

    adding columns and boards names/title

--- fake-trello/src/components/Board.js
+++ fake-trello/src/components/Board.js
@@ -1,32 +1,66 @@
 import { useIndexedDB } from "react-indexed-db";
 import React, { useState, useEffect } from "react";
-import { Button } from "@mantine/core";
+import { Button, Modal, TextInput } from "@mantine/core";
 import Column from "./Column";
-import { Grid } from "@mantine/core";
 
 export default function Board() {
   const { getAll } = useIndexedDB("columns");
   const { add } = useIndexedDB("columns");
+  const [opened, setOpened] = useState(false);
 
-  const [columns, setColumns] = useState({});
+  const [columns, setColumns] = useState([]);
+  const [columnTitle, setColumnTitle] = useState([]);
+
 
   useEffect(() => {
     getAll().then((columns) => {
       setColumns(columns);
     });
+    console.log(columns);
   }, []);
 
   const handleClick = () => {
-    add({ title: "title", cards: {} }).then((error) => {
+    // add({ title: "title", cards: {} }).then((error) => {
+    //   console.log(error);
+    // });
+    // window.location.reload();
+    setOpened(true);
+
+  };
+
+  function AddColumn(columnTitle) {
+    const { add } = useIndexedDB("columns");
+
+    add({
+      title: columnTitle,
+    }).then((error) => {
       console.log(error);
     });
-  };
+  }
 
   return (
     <div>
-      {Object.keys(columns).map((column) => (
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Column name"
+      >
+        <TextInput
+          placeholder="title"
+          weight={500}
+          onChange={(event) => setColumnTitle(event.currentTarget.value)}
+        ></TextInput>
+        <Button
+          onClick={() => {
+            AddColumn(columnTitle);
+            console.log(columnTitle)
+          }}
+        >
+          Set title
+        </Button>
+      </Modal>
+      {columns.map((column) => (
         <Column column={column} />
-        // <span key={columns}>{columns}</span>
       ))}
 
       <Button onClick={handleClick}>Add COLUMN</Button>
