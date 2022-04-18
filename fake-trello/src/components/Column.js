d70fd6a8900f933c58509e943843acd1723b8adb
Martin Vayer <martin.vayer@gmail.com>
2022-04-18 20:26:44 +0300

    add column delete action

--- fake-trello/src/components/Column.js
+++ fake-trello/src/components/Column.js
@@ -1,16 +1,34 @@
 import { Grid } from "@mantine/core";
-import { Card, Image, Text } from "@mantine/core";
+import { Card, Image, Text, Button } from "@mantine/core";
 import { SimpleGrid } from "@mantine/core";
 import Cards from "./Cards";
+import { useIndexedDB } from "react-indexed-db";
 
 export default function Column(props) {
-  console.log(props.column.name);
+  function DeleteColumn(columnId) {
+    const { deleteRecord } = useIndexedDB("columns");
+    deleteRecord(columnId).then((error) => {
+      console.log(error);
+    });
+  }
   return (
     <SimpleGrid>
       <Card shadow="sm" p="xl" component="a" target="_blank">
         <Card.Section>
           <p>{props.column.title}</p>
         </Card.Section>
+        <Button
+          variant="light"
+          color="red"
+          fullWidth
+          style={{ marginTop: 14 }}
+          onClick={() => {
+            DeleteColumn(props.column.id);
+            window.location.reload();
+          }}
+        >
+          delete
+        </Button>
       </Card>
       <Cards column={props.column} />
     </SimpleGrid>
