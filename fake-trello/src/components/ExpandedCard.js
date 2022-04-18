8d670e1a0e7fad5a937cf47b00a3fc0ccf732b88
Martin Vayer <martin.vayer@gmail.com>
2022-04-18 17:31:11 +0300

    improve card styling

--- fake-trello/src/components/ExpandedCard.js
+++ fake-trello/src/components/ExpandedCard.js
@@ -55,11 +55,6 @@ export default function ExpandedCard(props) {
   return (
     <Card
       style={{
-        width: "80%",
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
         border: "1px solid rgba(0, 0, 0, .3)",
         borderRadius: 10,
       }}
